7c43972dd200cad26963332985bc7f9b8c28da6a
Nikola <nikola.moricegallier@sevetys.fr>
2024-01-09 11:25:47 +0100

    fetch pokemon

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -1,15 +1,47 @@
+import Pokemon from "components/Pokemon/Pokemon"
 import styles from "./Home.module.css"
+import { useEffect, useState } from "react";
+
+type Pokemon = {
+   name: string
+   id: number
+   height: number
+   weight: number
+}
+
+function filterPokemonsByName(pokemons: Pokemon[], filteredName: string) {
+   return pokemons.filter(({ name }) => name.toLowerCase().includes(filteredName))
+}
 
 export const Home = () => {
-  return (
-    <div className={styles.intro}>
-      <div>Bienvenue sur ton futur pokédex !</div>
-      <div>Tu vas pouvoir apprendre tout ce qu'il faut sur React et attraper des pokemons !</div>
-      <div>
-        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/showdown/155.gif" alt="pokemon_155" />
-        <p>Name: Héricendre</p>
-        <p>Number: 155</p>
+   const [pokemonList, setPokemonList] = useState([])
+
+   const fetchPokemons = () => {
+      return fetch('http://localhost:8000/pokemons', { headers: { accept: "application/json" } })
+         .then(response => response.json())
+         .then(pokemonData => pokemonData);
+   }
+
+   useEffect(() => {
+      fetchPokemons().then((data) => setPokemonList(data))
+   }, [])
+
+   const [filterValue, setFilterValue] = useState('');
+
+   const onInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+      setFilterValue(event.target.value.toLowerCase());
+   };
+
+   return (
+      <div className={styles.intro}>
+         <div>Bienvenue sur ton futur pokédex !</div>
+         <div>Tu vas pouvoir apprendre tout ce qu'il faut sur React et attraper des pokemons !</div>
+         <input className={styles.input} onChange={onInputChange} value={filterValue} />
+         {
+            filterPokemonsByName(pokemonList, filterValue).map(({ name, id }) => {
+               return <Pokemon name={name} id={id} key={id} />
+            })
+         }
       </div>
-    </div>
-  )
+   )
 }
