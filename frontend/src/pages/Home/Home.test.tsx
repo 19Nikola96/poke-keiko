6f185e1d0759123ef980501f9861d7f231cf4d05
Ludovic Descateaux <ludovicd@hokla.com>
2024-01-10 12:06:16 +0100

    fix(Home.test.tsx): WIP fix tests config when mocking API calls

--- frontend/src/pages/Home/Home.test.tsx
+++ frontend/src/pages/Home/Home.test.tsx
@@ -1,48 +1,48 @@
 import { render, screen } from "@testing-library/react"
 import { Home } from "./index"
-import { http } from "msw"
+import { rest } from "msw"
 import { setupServer } from "msw/node"
 
-// const server = setupServer(
-// 	http.get("http://localhost:8000/pokemons", (req, res, ctx) => {
-// 		// Respond with a mocked user token that gets persisted
-// 		// in the `sessionStorage` by the `Login` component.
-// 		return res(
-// 			ctx.json([
-// 				{ id: 1, name: "bulbasaur", height: 7, weight: 69 },
-// 				{ id: 2, name: "ivysaur", height: 10, weight: 130 },
-// 			]),
-// 		)
-// 	}),
-// )
+const server = setupServer(
+  rest.get("http://localhost:8000/pokemons", (req, res, ctx) => {
+    // Respond with a mocked user token that gets persisted
+    // in the `sessionStorage` by the `Login` component.
+    return res(
+      ctx.json([
+        { id: 1, name: "bulbasaur", height: 7, weight: 69 },
+        { id: 2, name: "ivysaur", height: 10, weight: 130 },
+      ]),
+    )
+  }),
+)
 
-// describe("Test Fetching pokemon", () => {
-// 	// Enable API mocking before tests.
-// 	beforeAll(() => server.listen())
+describe("Test Fetching pokemon", () => {
+  // Enable API mocking before tests.
+  beforeAll(() => server.listen())
 
-// 	// Reset any runtime request handlers we may add during the tests.
-// 	afterEach(() => server.resetHandlers())
+  // Reset any runtime request handlers we may add during the tests.
+  afterEach(() => server.resetHandlers())
 
-// 	// Disable API mocking after the tests are done.
-// 	afterAll(() => server.close())
+  // Disable API mocking after the tests are done.
+  afterAll(() => server.close())
 
-// 	it("should display the list of the first 151 pokemons", () => {
-// 		console.log('croissant');
-// 		render(<Home />)
-// 		const bulbasaur = screen.findByText(/bulbasaur/)
-// 		const ivysaur = screen.findByText(/ivysaur/)
-// 		expect(ivysaur).toBeInTheDocument()
-// 		expect(bulbasaur).toBeInTheDocument()
-// 	})
-// })
-
-describe("<Home />", () => {
-	it("should display Héricendre", () => {
-		render(<Home />)
-		const hericendre = screen.getByText(/Héricendre/)
-		expect(hericendre).toBeInTheDocument()
-	})
+  it("should display the list of the first 151 pokemons", async () => {
+    render(<Home />)
+    const bulbasaur = await screen.findByText(/bulbasaur/)
+    const ivysaur = await screen.findByText(/ivysaur/)
+    console.log(screen.debug())
+    expect(ivysaur).toBeInTheDocument()
+    expect(bulbasaur).toBeInTheDocument()
+  })
 })
+
+// describe("<Home />", () => {
+//   it("should display Héricendre", () => {
+//     render(<Home />)
+//     const hericendre = screen.getByText(/Héricendre/)
+//     expect(hericendre).toBeInTheDocument()
+//   })
+// })
 
 // warning " > @testing-library/user-event@13.5.0" has unmet peer dependency "@testing-library/dom@>=7.21.4".
 // warning " > @typescript-eslint/eslint-plugin@5.4.0" has unmet peer dependency "eslint@^6.0.0 || ^7.0.0 || ^8.0.0".
--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -1,57 +1,67 @@
 import Pokemon from "components/Pokemon/Pokemon"
 import styles from "./Home.module.css"
-import { useEffect, useState } from "react";
+import { useEffect, useState } from "react"
 
 type PokemonList = {
-   pokemonList: Pokemon[]
-   filterValue: string
+  pokemonList: Pokemon[]
+  filterValue: string
 }
 
 type Pokemon = {
-   name: string
-   id: number
-   weight: number
-   height: number
+  name: string
+  id: number
+  weight: number
+  height: number
 }
 
 function filterPokemonsByName(pokemons: Pokemon[], filteredName: string) {
-   return pokemons.filter(({ name }) => name.toLowerCase().includes(filteredName))
+  return pokemons.filter(({ name }) => name.toLowerCase().includes(filteredName))
 }
 
 const fetchPokemons = async (url: string) => {
-   return fetch(url, { headers: { accept: "application/json" } })
-      .then(response => response.json())
-      .then(pokemonData => pokemonData);
-   //    const response = await fetch(url, { headers: { accept: "application/json" } })
-   //    const data = await response.json()
+  try {
+    const response = await fetch(url, { headers: { accept: "application/json" } })
+    const jsonResponse = await response.json()
+    return jsonResponse
+  } catch (error) {
+    console.log(error)
+    return []
+  }
 }
 
 export const Home = () => {
-   const [pokemonList, setPokemonList] = useState([])
-   const [filterValue, setFilterValue] = useState('');
+  const [pokemonList, setPokemonList] = useState([])
+  const [filterValue, setFilterValue] = useState("")
 
-   useEffect(() => {
-      fetchPokemons('http://localhost:8000/pokemons').then((data) => setPokemonList(data))
-   }, [])
+  const updatePokemonList = async () => {
+    const pokemonList = await fetchPokemons("http://localhost:8000/pokemons")
+    setPokemonList(pokemonList)
+  }
 
-   return (
-      <div className={styles.intro}>
-         <h1>Pokedex !</h1>
-         <input className={styles.input} placeholder="Search" onChange={(e) => setFilterValue(e.target.value.toLowerCase())} value={filterValue} />
-         <PokemonList pokemonList={pokemonList} filterValue={filterValue} />
-      </div>
-   )
+  useEffect(() => {
+    updatePokemonList()
+  }, [])
 
+  return (
+    <div className={styles.intro}>
+      <h1>Pokedex !</h1>
+      <input
+        className={styles.input}
+        placeholder="Search"
+        onChange={e => setFilterValue(e.target.value.toLowerCase())}
+        value={filterValue}
+      />
+      <PokemonList pokemonList={pokemonList} filterValue={filterValue} />
+    </div>
+  )
 }
 
 const PokemonList = ({ pokemonList, filterValue }: PokemonList) => {
-   return (
-      <div className={styles.cardList}>
-         {
-            filterPokemonsByName(pokemonList, filterValue).map(({ name, id, weight, height }) => {
-               return <Pokemon name={name} id={id} key={id} weight={weight} height={height} />
-            })
-         }
-      </div>
-   )
+  return (
+    <div className={styles.cardList}>
+      {filterPokemonsByName(pokemonList, filterValue).map(({ name, id, weight, height }) => {
+        return <Pokemon name={name} id={id} key={id} weight={weight} height={height} />
+      })}
+    </div>
+  )
 }
